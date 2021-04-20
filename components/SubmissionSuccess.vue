32ee2acf672ca418869c99a0000b37504da18db4
Danny Child <dannychild.contact@gmail.com>
2021-04-20 16:54:02 -0600

    Create SubmissionSuccess.vue

---
+++ components/SubmissionSuccess.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Thanks you! Your submission has been recorded!</h1>
+    <router-link to="/">
+      <button>Back to form</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SubmissionSuccess"
+  }
+</script>
+
+<style scoped>
+</style>
