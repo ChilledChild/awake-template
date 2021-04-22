f1f6ee9a7bd3bf5ad8507d2b454d4cf3a0939c6d
Danny Child <dannychild.contact@gmail.com>
2021-04-22 12:06:50 -0600

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -55,7 +55,7 @@ export default {
   disqus: {
     on: false,
     loadingStrategy: 'button', // Options: onload, lazy, button
-    siteShortName: 'blog-danielkelly-io' // 'blog-danielkelly-io' change this out for my own later
+    siteShortName: '' // 'blog-danielkelly-io' change this out for my own later
   },
 
   // Newsletter Subscribe
@@ -68,7 +68,7 @@ export default {
     mailchimp: {
       on: false,
       formAction:
-        'https://danielkelly.us3.list-manage.com/subscribe/post?u=d3c2e762b66a2353d0bc29bf3&amp;id=2abeaa06e0' // change this out for my own later
+        '' // change this out for my own later
     },
     custom: {
       on: false,
